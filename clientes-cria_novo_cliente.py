fb4be9ffffe88d62f3130f424393ad7e4618024f
Wanna Maise <a82952@ualg.pt>
2023-11-23 16:34:44 +0000

    adicionei um codigo cria_novo_cliente

---
+++ clientes-cria_novo_cliente.py
@@ -0,0 +1,14 @@
+def cria_novo_cliente():
+    nome = input('Nome Completo: ')
+    numero = input('Número de Contato: ')
+    morada = input('Morada: ')
+    Nmorada =input('Nº de morada: ')
+    cp = input('Código Postal: ')
+    nif = input('NIF: ')
+    mail = input('Email: ')
+
+    N_cliente = (f" Nome: {nome}\n Número de Contato: {numero}\n Morada: {morada}\n Nº de Morada: {Nmorada}\n Código Postal: {cp}\n NIF: {nif}\n Email: {mail}")
+    
+    return N_cliente
+
+print(cria_novo_cliente())
