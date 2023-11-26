2b1ee2a84ae3ca9e52611a13d2c4f0c103f9ffe5
Wanna Maise <a82952@ualg.pt>
2023-11-26 15:32:28 +0000

    função para criar uma lista de clientes

---
+++ clientes-imprime_lista_de_clientes.py
@@ -0,0 +1,4 @@
+def imprime_lista_de_clientes(lista_de_clientes):
+  "recebe os dados guardados na lista de cliente e chama a função para imprimir e criar uma tabela"
+
+  imprime_lista(cabecalho="Lista de Clientes", lista=lista_de_clientes)
