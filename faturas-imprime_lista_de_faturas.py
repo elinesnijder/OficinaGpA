2984c1fb7dd54ff6d792cb854e24c780f9af0a0d
Wanna Maise <a82952@ualg.pt>
2023-11-26 16:12:47 +0000

    função para imprimir a lista de faturas

---
+++ faturas-imprime_lista_de_faturas.py
@@ -0,0 +1,5 @@
+def imprime_lista_de_faturas(lista_de_faturas):
+    """recebe os dados guardados na lista de faturas e chama a função para imprimir e 
+    criar uma tabela"""
+
+    imprime_lista(cabecalho="Lista de Faturas", lista=lista_de_faturas)
