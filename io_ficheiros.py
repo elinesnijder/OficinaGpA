475e1ba5c24b328f83b90deb0d55fcf18313fd08
elinesnijder <a84785@ualg.pt>
2023-11-26 16:12:52 +0000

    Adicionei guarda_as_listas ao io_ficheiros

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -5,11 +5,23 @@ from faturas import nome_ficheiro_lista_de_faturas
 
 from veiculos import nome_ficheiro_lista_de_veiculos
 
+def carrega_as_listas_dos_ficheiros():
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
+    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_veiculos)
+    lista_de_clientes = le_de_ficheiro(nome_ficheiro_lista_de_clientes)
+    lista_de_faturas = le_de_ficheiro(nome_ficheiro_lista_de_faturas)
 
-# ...
+    return  lista_de_veiculos, lista_de_clientes, lista_de_faturas
+
+def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas):
+
+    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
+    if op in ['s', 'S']:
+        guarda_em_ficheiro(nome_ficheiro_lista_de_veiculos, lista_de_veiculos)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_clientes, lista_de_clientes)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_faturas, lista_de_faturas)
+    else:
+        print("Gravação cancelada...")
+
+
+
