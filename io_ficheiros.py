c7175d2eeaeefc25be1362d029c8e6f4a273aeb8
elinesnijder <a84785@ualg.pt>
2023-11-26 16:17:25 +0000

    Adicionei le_de_ficheiros ao io_ficheiros

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -28,5 +28,10 @@ def guarda_em_ficheiro(nome_do_ficheiro, dados):
     with open(nome_do_ficheiro, "wb") as f:
         pickle.dump(dados, f)
 
+def le_de_ficheiro(nome_ficheiro):
+
+    with open(nome_ficheiro, "rb") as f:
+        return pickle.load(f)
+
 
 
