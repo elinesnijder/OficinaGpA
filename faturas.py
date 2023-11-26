f7b9841150229002ececd453ec78d344ff7dac8b
Wanna Maise <a82952@ualg.pt>
2023-11-26 16:24:55 +0000

    funções para criar nova fatura

--- faturas.py
+++ faturas.py
@@ -1,9 +1,11 @@
-def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
-    """Pede ao utilizador para introduzir os dados de uma nova fatura
+from datetime import date
 
-    :return: dicionario com uma fatura, na forma
-        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
-    """
+from io_terminal import imprime_lista, pergunta_id
+
+nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
+
+def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
+
     id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
     id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
     descricaos_serviço = print(input("Resumo de serviço: "))
@@ -18,3 +20,6 @@ def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
 
     return fatura
 
+def imprime_lista_de_faturas(lista_de_faturas):
+    
+    imprime_lista(cabecalho="Lista de Faturas", lista=lista_de_faturas)
